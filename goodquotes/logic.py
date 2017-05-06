64bfbd86cb110a101bd28f841fc008505ed53465
Kshitij Saraogi <KshitijSaraogi@gmail.com>
2017-05-06 22:41:24 +0530

    added random quote generators

---
+++ goodquotes/logic.py
@@ -0,0 +1,22 @@
+import random
+import csv
+
+from .parser import search_quotes
+
+def random_book(file):
+    book_title, book_author = None, None
+    with open(file, 'r') as f:
+        reader = csv.reader(f)
+        data = list(reader)
+        num_of_books = len(data) - 1
+
+        selected_book_id = random.randint(1, num_of_books)
+        selected_book = data[selected_book_id]
+        book_title, book_author = selected_book[1], selected_book[2]
+
+    return book_title, book_author
+
+def random_quote(book_title, book_author)
+    book_quotes = search_quotes(book_title)
+    random_quote = random.choice(book_quotes)
+    return random_quote
