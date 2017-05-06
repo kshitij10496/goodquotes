5d3f8c519317db0dc6d446f4f3bb48653c61b998
Kshitij Saraogi <KshitijSaraogi@gmail.com>
2017-05-06 23:48:40 +0530

    text wrapping quotes

--- goodquotes/cli.py
+++ goodquotes/cli.py
@@ -3,7 +3,7 @@ import click
 
 from .logic import random_quote, random_book
 
-CONFIG_FILE = os.path.expanduser('~') + './goodquotes'
+CONFIG_FILE = os.path.expanduser('~') + '/.goodquotes'
 
 @click.group()
 def cli():
@@ -19,9 +19,9 @@ def quote_book(book):
 
     elif os.path.exists(CONFIG_FILE):
         with open(CONFIG_FILE, 'r') as config_file:
-            KINDLE_LIBRARY = config_file.readline()
+            KINDLE_LIBRARY = (config_file.readline().split('=')[1]).strip()
 
-        selected_title, selected_author  = random_book(KINDLE_LIBRARY)
+        selected_title, selected_author = random_book(KINDLE_LIBRARY)
         rquote = random_quote(selected_title, selected_author)
         click.echo(rquote)
 
@@ -35,8 +35,8 @@ def import_library(filepath):
         _, file_ext = os.path.splitext(filepath)
         if file_ext == '.csv':
             KINDLE_LIBRARY = os.path.abspath(filepath)
-            with open(CONFIG_FILE, 'w') as config_file:
-                config_file.write('KINDLE_LIBRARY = {}'.format(KINDLE_LIBRARY))
+            with open(CONFIG_FILE, 'w+') as config_file:
+                config_file.write('KINDLE_LIBRARY={}'.format(KINDLE_LIBRARY))
             # store the path in app config
         else:
             click.echo('It seems that you have entered an incorrect file.')
