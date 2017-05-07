d9d8299837af261201cb5cc1bf1d67d6f7b534f1
Kshitij Saraogi <KshitijSaraogi@gmail.com>
2017-05-07 15:01:03 +0530

    added options in place of subcommands

--- goodquotes/cli.py
+++ goodquotes/cli.py
@@ -5,14 +5,29 @@ from .logic import random_quote, random_book
 
 CONFIG_FILE = os.path.expanduser('~') + '/.goodquotes'
 
-@click.group()
-def cli():
-    pass
 
-@cli.command('book')
-@click.argument('book', nargs=-1)
-def quote_book(book):
-    book_title = ' '.join(book)
+@click.command()
+@click.option('--book', '-b', default=None, help='Title of the book')
+@click.option('--library', '-l', default=None, help='Path to imported Goodreads library')
+def cli(book, library):
+    """ Generates a quote at random from your library.
+    Given a book, it prints the quote specific to this book.
+    """
+    if library is not None and book is not None:
+        click.echo('It seems that you are trying to perform two operations at the same time. But do not worry, we have got you covered, as always.')
+        click.echo("We will import your library while you enjoy a memorable moment from '{}'.")
+
+    if library is not None:
+        import_library(library)
+
+    if book is not None:
+        quote_book(book)
+
+    if book is None and library is None:
+        quote_book(None)
+
+
+def quote_book(book_title):
     if book_title:
         rquote = random_quote(book_title)
         click.echo(rquote)
@@ -28,8 +43,7 @@ def quote_book(book):
     else:
         click.echo("You have to either import your Goodreads Library or tell me about your favourite novel! I will listen intently. :)")
 
-@cli.command('import')
-@click.argument('filepath')
+
 def import_library(filepath):
     if os.path.isfile(filepath):
         _, file_ext = os.path.splitext(filepath)
@@ -38,10 +52,10 @@ def import_library(filepath):
             with open(CONFIG_FILE, 'w+') as config_file:
                 config_file.write('KINDLE_LIBRARY={}'.format(KINDLE_LIBRARY))
             # store the path in app config
+            click.echo('Successfully imported your library. :)\nExecute "goodquotes" to relive moments from these books.')
         else:
-            click.echo('It seems that you have entered an incorrect file.')
+            click.echo('We are having trouble importing your library. It seems that you have entered an incorrect file.')
             click.echo('Enter a valid csv file imported from Goodreads.')
 
     else:
-        click.echo('It seems that you have entered an incorrect file.')
-        click.echo('Enter a valid csv file imported from Goodreads.')
+        click.echo('The path to the file does not exist. Kindly check the entered file path.')
